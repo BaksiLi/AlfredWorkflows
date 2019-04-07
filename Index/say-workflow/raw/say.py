f01f379881f3f88fcc914228a3d49e0d21b5af2d
BaksiLi <myself@baksili.codes>
2019-04-08 00:55:25 +0100

    Compatible with random capitalisations
    of voices. for example 'jP' and 'cH'.

--- Index/say-workflow/raw/say.py
+++ Index/say-workflow/raw/say.py
@@ -1,22 +1,27 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*
-__version__ = '0.2_alfred'
+__version__ = '0.3_alfred'
 import os
 import sys
 
-langs = {'jp':'Kyoko','en':'Daniel','ch':'Sin-ji','fr':'Thomas'}
-
 query = sys.argv[1]
 
-if "'" in query:
-    query = query.replace("'", "’")
+if "’" in query:
+    query = query.replace("’", "'")
 
-recog = query[0] + query[1]
-if recog in langs:
-    says = '-v ' + langs[recog] + ' '
+voices = {'en':'Daniel','jp':'Kyoko','ch':'Sin-ji','fr':'Thomas', 'kr': 'Yuna'}
+voice_default = 'en'  # not `really default'
+
+try:  # try to recognise any voice names
+    recog = query[:2].lower()  # the first two letters
+except IndexError:
+	recog = voice_default  # if failed, use the `default voice'
+
+if recog in voices:
+    says = '-v ' + voices[recog] + ' '
     for i in range(2, len(query)):
         says += query[i]
-elif query == 'who you are':
+elif query == '?':  # Easter Egg!
     says = 'I am your assistant.'
 else:
     says = query
